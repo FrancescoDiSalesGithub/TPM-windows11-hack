dc743571caf5f80183cc529d792bfe7456f0e4d6
Francesco Di Sales <f.disales@hotmail.com>
2021-10-29 23:24:12 +0200

    Create tpmpwn.bat

---
+++ tpmpwn.bat
@@ -0,0 +1,3 @@
+reg add HKLM\system\setup\LabConfig /v BypassTPMCheck /d 1
+reg add HKLM\system\setup\LabConfig /v BypassRAMCheck /d 1
+reg add HKLM\system\setup\LabConfig /v Bypasssecurebootcheck /d 1
